e1b8946732464f9ff9ad4fadff4d12afa72d3c98
iliach1997 <90194981+iliach1997@users.noreply.github.com>
2022-04-10 14:56:58 +0400

    *lllll*

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export const NoMatch=()=>{
     return(
         <div>
-            <h1>eror 401;</h1>
+            <h1>eror 4;</h1>
             <Link to="/home">go to the hose</Link>
         </div>
     )
